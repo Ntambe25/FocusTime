f4b9719eebbac2ff2cf6e74afdab02f2cce3a35c
Nahush Tambe <Nahushtambe@gmail.com>
2023-03-01 08:43:52 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import Constants from 'expo-constants';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <View>
+          <Text style={{ colo: colors.white }}>
+            I am going to render the Timer for {currentSubject}
+          </Text>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkBlue,
+  },
+});
